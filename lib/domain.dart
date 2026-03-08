df561fe706db852375b8c1f107ea0e9c31fad9b2
Lukasz Walewski <lukasz.walewski@proton.me>
2026-03-08 19:11:09 +0100

    Move Event to separate source file

--- lib/domain.dart
+++ lib/domain.dart
@@ -1,37 +1,5 @@
 import 'package:flutter/foundation.dart';
-
-enum EventSource {
-  port("Port"),
-  engine("Engine"),
-  sail("Sail"),
-  anchor("Anchor");
-
-  final String label;
-
-  const EventSource(this.label);
-}
-
-enum EventType { start, stop }
-
-class Event {
-  final EventSource source;
-  final EventType type;
-  final DateTime timestamp;
-  Event({required this.source, required this.type, required this.timestamp});
-}
-
-class Trip extends ChangeNotifier {
-  final List<Event> events = [];
-
-  Trip() {
-    addEvent(EventSource.port, EventType.start);
-  }
-
-  void addEvent(EventSource source, EventType type) {
-    events.add(Event(source: source, type: type, timestamp: DateTime.now()));
-    notifyListeners();
-  }
-}
+import 'package:hello/event.dart';
 
 enum Mode {
   sailing("Sailing"),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello/event.dart';
 
 import 'domain.dart';
 
